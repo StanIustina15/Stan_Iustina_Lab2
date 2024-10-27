60a05857878b4770ce09329a16f7e8e4fad39972
IUSTINA-ALEXANDRA STAN <iustina.stan@stud.ubbcluj.ro>
2024-10-27 19:44:55 +0200

    Laborator3

--- Models/Book.cs
+++ Models/Book.cs
@@ -17,5 +17,6 @@ namespace Stan_Iustina_Lab2.Models
         public Publisher? Publisher { get; set; }
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
+        public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stan_Iustina_Lab2.Data;
+using Stan_Iustina_Lab2.Models;
+
+namespace Stan_Iustina_Lab2.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Stan_Iustina_Lab2.Data.Stan_Iustina_Lab2Context _context;
+
+        public IndexModel(Stan_Iustina_Lab2.Data.Stan_Iustina_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
