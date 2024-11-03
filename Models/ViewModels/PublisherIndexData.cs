08dcd93ac548280d03951fa6257ca895da0ad3e0
IUSTINA-ALEXANDRA STAN <iustina.stan@stud.ubbcluj.ro>
2024-11-03 17:55:29 +0200

    Laborator4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Stan_Iustina_Lab2.Models;
+namespace Stan_Iustina_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Stan_Iustina_Lab2.Data;
 using Stan_Iustina_Lab2.Models;
+using Stan_Iustina_Lab2.Models.ViewModels;
 
 namespace Stan_Iustina_Lab2.Pages.Categories
 {
@@ -21,9 +22,29 @@ namespace Stan_Iustina_Lab2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(c => c.BookCategories)
+                    .ThenInclude(bc => bc.Book)
+                    .ThenInclude(b => b.Author)
+                .OrderBy(c => c.CategoryName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                var selectedCategory = CategoryData.Categories
+                    .Where(c => c.ID == id.Value).Single();
+                CategoryData.Books = selectedCategory.BookCategories
+                    .Select(bc => bc.Book);
+            }
         }
     }
 }
+    
+
